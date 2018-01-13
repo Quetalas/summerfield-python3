7297c3b2a7162fa1f0d5fb45270f52cae5afea7e
Quetalas <evgeny.kruzhckov@yandex.ru>
2018-01-13 21:53:17 +0300

    add me first test

--- chapter2/csv2html_test.py
+++ chapter2/csv2html_test.py
@@ -1,1 +1,11 @@
+import csv2html
 import unittest
+
+
+class TestProcessOptionsMethod(unittest.TestCase):
+
+    def test_less(self):
+        self.assertEqual(csv2html.process_options(),(100, '.0f'))
+
+if __name__ == '__main__':
+    unittest.main()
